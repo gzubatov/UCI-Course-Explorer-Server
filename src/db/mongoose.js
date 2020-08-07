4f38c0fa2ca481492c878d1eb5010c2fe19de920
Greg Zubatov <gzubatov@gmail.com>
2020-08-06 22:28:21 -0700

    Update db url for deployment

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env
-	.DB_PASSWORD}@cluster0.hsnam.mongodb.net/${process.env
+	.DB_PASSWORD}@zotreviewsdb.haje3.mongodb.net/${process.env
 	.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(url, {
